f56a86a80eb8b6c5077877b7a0c307e6617ab0f3
1 <1.com>
2022-10-11 20:26:23 +0800

    1

---
+++ Hello1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("176+678=%d",176+678);
+	
+	return 0;
+}
---
+++ hehe.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	printf("12%11=%d",12%11);
+	return 0;
+}
